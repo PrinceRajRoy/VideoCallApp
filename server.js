37d9dad28d326355fb3c452eafa582c28b8e2c96
Prince Raj Roy <princelikesall16@gmail.com>
2020-10-26 02:14:46 +0530

    Path Require (Server.js)

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
-})
+// app.get('/:room', (req, res) => {
+//     res.render('room', { roomId: req.params.room })
+// })
 
 io.on('connection', socket => {
     socket.on('join', (roomId, peerId, name) => {
